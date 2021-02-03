91432ad73ce1fbebad0c1b93900a3848d98fab44
Caleb John <cajohn0205@gmail.com>
2021-02-02 16:47:16 -0800

    saving

--- wall_follow.py
+++ wall_follow.py
@@ -2,11 +2,74 @@ import RPi.GPIO as GPIO
 import json
 import threading
 import VL53L0X
-import time, sys
+import sys
+import time
+import serial
 
 
 from PointOS.movement import motor_control
 
+# Establish the connection on a specific port
+IMU = serial.Serial(
+    '/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 115200)
+IMU.readline()
+IMU.readline()
+
+for i in range(100):
+    imu_raw = (str(IMU.readline())).split(',')
+    imu_raw[0] = imu_raw[0][2:]
+    imu_raw[-1] = imu_raw[-1][:-5]
+    try:
+        heading = float(imu_raw[0])
+        gyro_x = float(imu_raw[1])
+        gyro_y = float(imu_raw[2])
+        accel_x = float(imu_raw[3])
+        accel_y = float(imu_raw[4])
+        print('----------')
+        print("heading: " + str(heading))
+        print("gyro_x: " + str(gyro_x))
+        print("gyro_y: " + str(gyro_y))
+        print("accel_x: " + str(accel_x))
+        print("accel_y: " + str(accel_y))
+        print('----------')
+    except Exception as e:
+        print(e)
+
+
+IMU.close()
+
+# Establish the connection on a specific port
+st = time.time()
+IMU = serial.Serial(
+    '/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 115200)
+IMU.readline()
+end = time.time() - st
+print("elapse: " + str(end))
+time.sleep(10)
+IMU.readline()
+
+
+for i in range(100):
+    imu_raw = (str(IMU.readline())).split(',')
+    imu_raw[0] = imu_raw[0][2:]
+    imu_raw[-1] = imu_raw[-1][:-5]
+    try:
+        heading = float(imu_raw[0])
+        gyro_x = float(imu_raw[1])
+        gyro_y = float(imu_raw[2])
+        accel_x = float(imu_raw[3])
+        accel_y = float(imu_raw[4])
+        print('----------')
+        print("heading: " + str(heading))
+        print("gyro_x: " + str(gyro_x))
+        print("gyro_y: " + str(gyro_y))
+        print("accel_x: " + str(accel_x))
+        print("accel_y: " + str(accel_y))
+        print('----------')
+    except Exception as e:
+        print(e)
+
+IMU.close()
 
 pin_map_file = open('/home/pi/PointOS/res/pinout.json')
 pin_map = dict(json.load(pin_map_file))
@@ -26,20 +89,22 @@ laser = pin_map['output']['laser']
 
 
 # GPIO for Sensor 1 shutdown pin
-sensor1_shutdown = 23
+sensor1_shutdown = pin_map['output']['lidar0_shutdown']
 # GPIO for Sensor 2 shutdown pin
-sensor2_shutdown = 18
+sensor2_shutdown = pin_map['output']['lidar1_shutdown']
 
+
+'''
 
 GPIO.setup(sensor1_shutdown, GPIO.OUT)
 GPIO.setup(sensor2_shutdown, GPIO.OUT)
 
 # Set all shutdown pins low to turn off each VL53L0X
-GPIO.output(sensor1_shutdown, GPIO.LOW)
-GPIO.output(sensor2_shutdown, GPIO.LOW)
+GPIO.output(sensor1_shutdown, 0)
+GPIO.output(sensor2_shutdown, 0)
 time.sleep(0.1)
-GPIO.output(sensor1_shutdown, GPIO.LOW)
-GPIO.output(sensor2_shutdown, GPIO.HIGH)
+GPIO.output(sensor1_shutdown, 0)
+GPIO.output(sensor2_shutdown, 1)
 time.sleep(0.1)
 
 
@@ -52,7 +117,7 @@ tof.open()
 # Start ranging
 tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-GPIO.output(sensor1_shutdown, GPIO.HIGH)
+GPIO.output(sensor1_shutdown, 1)
 time.sleep(0.1)
 tof1 = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
 
@@ -60,15 +125,15 @@ tof1.open()
 # Start ranging
 tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
+#1 is back sensor, 0 is the front one
 
-
-for count in range(1, 101):
+for count in range(1, 10):
     distance = tof1.get_distance()
     distance1 = tof.get_distance()
-    print("s0: " + str(distance) + " mm")
-    print("s1: " + str(distance1) + " mm")
 
-    time.sleep(0.001)
+    
+    time.sleep(0.1)
+
 
 tof.stop_ranging()
 tof.close()
@@ -76,12 +141,11 @@ tof.close()
 tof1.stop_ranging()
 tof1.close()
 
-
+'''
 
 
 motor_control.set_direction('b')
 motor_control.set_motor_res('1/2')
-
 
 
 '''
@@ -95,7 +159,6 @@ for i in range(10000):
 
 motor_control.motor_disable()
 '''
-
 
 
 GPIO.cleanup()
