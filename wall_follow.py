7083fbb00725237d650c214f735272fb14bd0a0e
Caleb John <cajohn0205@gmail.com>
2021-02-28 17:56:41 -0800

    new servo based PID

--- wall_follow.py
+++ wall_follow.py
@@ -6,7 +6,7 @@ import time
 
 
 from PointOS.movement import motor_control
-from PointOS.sensors import imu
+from PointOS.sensors import imu, lidar
 
 
 pin_map_file = open('/home/pi/PointOS/res/pinout.json')
@@ -22,6 +22,12 @@ l_dir = pin_map['output']['l_dir']
 
 r_step = pin_map['output']['r_step']
 l_step = pin_map['output']['l_step']
+
+steer_straight_pin = pin_map['output']['straight_pin']
+steer_right_pin = pin_map['output']['right_pin']
+steer_left_pin = pin_map['output']['left_pin']
+steer_turn_pin = pin_map['output']['turn_pin']
+
 
 laser = pin_map['output']['laser']
 
@@ -39,14 +45,24 @@ global imu_flag
 imu_flag = 0
 
 global delays
-delays = [0.00015,0.00015]# r,l
+delays = [0.0005,0.0005]# r,l
 delay_r = 0.0005
 delay_l = 0.0005
 
 global step_count
 step_count = [0,0]#0 index Left, 1 index Right
 
+global lidar_data
+global lidar_flag
 
+lidar_data = [-1.0, -1.0]
+lidar_flag = 0
+
+lidar_data[0] = lidar.get_lidar_1()# front sensor
+lidar_data[1] = lidar.get_lidar_2()# back sensor
+
+lidar_data[0] = lidar.get_lidar_1()
+lidar_data[1] = lidar.get_lidar_2()
 
 global prev_errors
 
@@ -60,6 +76,39 @@ current_imu_data = imu.get_imu_data()
 
 motor_control.set_motor_res('1/2')
 motor_control.motor_enable()
+start_1 = lidar_data[0]
+start_2 = lidar_data[1]
+
+
+if(start_1 > start_2): #turn right
+    motor_control.set_direction('r')
+    diff = lidar_data[0] - lidar_data[1]
+    if(diff > 1.0):
+        while(lidar_data[0] - lidar_data[1] > 0):
+            for i in range(25):
+                motor_control.move_motors(0.00015)
+                
+            lidar_data[0] = lidar.get_lidar_1()
+            lidar_data[1] = lidar.get_lidar_2()
+
+
+elif(start_2 > start_1): # turn left
+    motor_control.set_direction('l')
+    diff = lidar_data[1] - lidar_data[0]
+    if(diff > 1.0):
+        while(lidar_data[1] - lidar_data[0] > 0):
+            for i in range(25):
+                motor_control.move_motors(0.00015)
+
+            lidar_data[0] = lidar.get_lidar_1()
+            lidar_data[1] = lidar.get_lidar_2()
+
+
+start_1 = lidar_data[0]
+start_2 = lidar_data[1]
+
+
+
 def read_imu():
     while(imu_flag == 0):
         imu_arr = imu.get_imu_data()
@@ -72,54 +121,75 @@ def read_imu():
 
 
 
-def move_right_motor():
-    for i in range(60000):
-        motor_control.move_right_motor(delays[0])
+def move_motors():
+    for i in range(20000):
+        motor_control.move_motors(0.00015)
         step_count[0] = i
     return
 
 
-def move_left_motor():
-    for i in range(60000):
-        motor_control.move_left_motor(delays[1])
-        step_count[1] = i
+
+
+
+def read_lidars():
+    KP = 0.000000018
+    KD = 0.000000004
+    KI = 0.00000000002
+    lidar_tgt = int((lidar_data[0] + lidar_data[1])/2)
+    while(lidar_flag == 0):
+        lidar_data_1 = lidar.get_lidar_1()
+        lidar_data_2 = lidar.get_lidar_2()
+        lidar_score = int((lidar_data_1 + lidar_data_2)/2)
+
+        current_error = lidar_tgt - lidar_score
+        adj = (current_error * KP) + (prev_errors[0] * KD) + (prev_errors[1] * KI)
+        if(adj > 0): # slow down right motor
+            if(delays[1] > 0.0002):# max left motor speed
+                delays[1] -= adj
+                delays[0] += adj
+        if(adj < 0):# slow down left motor
+            if(delays[0] > 0.0002):# max right motor speed
+                delays[0] -= adj
+                delays[1] += adj
+
+        print(current_error)
+        print(str(delays[1]) + ' l')
+        print(str(delays[0]) + ' r')
+
     return
 
 
-
-
-
 imu_thread = threading.Thread(target=read_imu, args=())
-
+lidar_thread = threading.Thread(target=read_lidars, args=())
 imu_thread.start()
+lidar_thread.start()
 
 
 
-
-motor_control.set_direction('b')
-motor_control.set_motor_res('1/2')
+motor_control.set_direction('f')
+motor_control.set_motor_res('1/4')
 motor_control.motor_enable()
 
-right_motor_thread = threading.Thread(
-    target=move_right_motor, args=())
-left_motor_thread = threading.Thread(
-    target=move_left_motor, args=())
+
+motor_thread = threading.Thread(
+    target=move_motors, args=())
 
 
+
+
+print(lidar_data)
 print(current_imu_data)
-right_motor_thread.start()
-left_motor_thread.start()
+motor_thread.start()
+motor_thread.join()
 
-right_motor_thread.join()
-left_motor_thread.join()
 motor_control.motor_disable()
-imu_flag = 1
-
+print(lidar_data)
 print(current_imu_data)
 
-print(step_count)
+imu_flag = 1
+lidar_flag = 1
 imu_thread.join()
-
+lidar_thread.join()
 
 
 GPIO.cleanup()
